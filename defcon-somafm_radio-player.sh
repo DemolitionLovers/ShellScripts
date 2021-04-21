fee1e8f4ed452fd4365d5099bda2b9bf1f3c4055
DemolitionLovers <khannahk@protonmail.com>
2021-04-21 11:40:12 +0100

    Some scripts using the mpv media player to play internet radio streams.

---
+++ defcon-somafm_radio-player.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+url="https://somafm.com/defcon32.pls"
+agent_string='Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0'
+agent_print='Tor Browser (20201007)'
+
+if [ $# -eq 1 ]; then
+
+  echo "--- Radio Station: Defcon @SomaFM"
+  echo "--- User Agent: $agent_print"
+
+  vol_var="$1"
+  check_fail=0
+
+  while [ "$check_fail" -eq 0 ]; do
+    mpv --volume=$vol_var\
+        --user-agent="$agent_string"\
+        --force-seekable=yes $url || check_fail=1
+  done
+
+else
+  echo "check arguments"
+fi
